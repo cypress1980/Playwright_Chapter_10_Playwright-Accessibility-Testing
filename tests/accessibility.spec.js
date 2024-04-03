7223d1e56957865f3725ea79942733cc01b83ae3
Kailash Chander Pathak <you@example.com>
2024-04-03 20:05:16 +0530

    Added more accessbility tc

--- tests/accessibility.spec.js
+++ tests/accessibility.spec.js
@@ -1,6 +1,5 @@
 const { test, expect } = require("@playwright/test");
 import AxeBuilder from "@axe-core/playwright"; 
-import checkA11y from "@axe-core/playwright"; 
 
 test("Scan the Complete Page", async ({page}) => {
   await page.goto("https://www.thetesttribe.com/my-account/edit-account/");
@@ -8,9 +7,22 @@ test("Scan the Complete Page", async ({page}) => {
   expect(accessibilityScanResults.violations).toEqual([]); 
 });
 
-test("Scan the Complete Pag related to WCAG A or AA violations", async ({page}) => {
+test("Scan the Partial Page Include Login Credentials Section during Scan", async ({page}) => {
+  await page.goto("https://www.thetesttribe.com/my-account/edit-account/");
+  const accessibilityScanResults = await new AxeBuilder({ page }).include('.woocommerce').analyze();
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
+
+test("Scan the Complete Page related to WCAG A or AA violations", async ({page}) => {
   await page.goto("https://www.thetesttribe.com/my-account/edit-account/");
   const accessibilityScanResults = await new AxeBuilder({ page }).withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"]).analyze();
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
+
+
+test("Scan the Complete Page related to WCAG A or AA violations with disabling the rule 'region'", async ({page}) => {
+  await page.goto("https://www.thetesttribe.com/my-account/edit-account/");
+  const accessibilityScanResults = await new AxeBuilder({ page }).withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"]).disableRules(["region"]).analyze();
   expect(accessibilityScanResults.violations).toEqual([]);
 });
 
@@ -34,9 +46,25 @@ test("Include Password Field ONLY during Scan", async ({page}) => {
   const accessibilityScanResults = await new AxeBuilder({ page }).include('#password').analyze();
   expect(accessibilityScanResults.violations).toEqual([]);
 });
-test("Exclude login_credentials during Scan", async ({page}) => {
+
+test("Scan only the Username text field of the Login Credentials Screen", async ({page}) => {
   await page.goto("https://www.thetesttribe.com/my-account/edit-account/");
-  const accessibilityScanResults = await new AxeBuilder({ page }).exclude('.section top-bnr-section').analyze();
+  const accessibilityScanResults = await new AxeBuilder({ page }).include('#username').analyze();
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
+test("Exclude Top Banner Section Scan", async ({page}) => {
+  await page.goto("https://www.thetesttribe.com/my-account");
+  const accessibilityScanResults = await new AxeBuilder({ page }).exclude('#termly-code-snippet-support').analyze();
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
+test("Exclude Login Tab during Scan", async ({page}) => {
+  await page.goto("https://www.thetesttribe.com/my-account");
+  const accessibilityScanResults = await new AxeBuilder({ page }).exclude('[data-id="tab1"]').analyze();
   expect(accessibilityScanResults.violations).toEqual([]);
 });
 
+test("Disable Rules login_credentials during Scan", async ({page}) => {
+  await page.goto("https://www.thetesttribe.com/my-account/edit-account/");
+  const accessibilityScanResults = await new AxeBuilder({ page }).disableRules(["region"]).analyze();
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
